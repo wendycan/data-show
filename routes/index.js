3b2c1a348bfded04bc6926953eda3379713e0401
Wendy Wang <wilxnwang@gmail.com>
2014-12-09 18:25:08 +0800

    Add areas

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,45 @@ var sqlite3 = require("sqlite3").verbose();
 var router = express.Router();
 
 var file = "itjuzi";
+var areas = [
+  '移动互联网',
+  '电子商务',
+  'SNS社交网络',
+  '广告营销',
+  '搜索引擎',
+  '游戏动漫',
+  '电子硬件',
+  '媒体资讯',
+  '多媒体娱乐工具',
+  '软件消费',
+  '生活金融服务',
+  '医疗健康',
+  '企业服务',
+  '旅游户外',
+  '房产酒店',
+  '文化艺术体育',
+  '教育培训',
+  '汽车交通',
+  '法律法务',
+  '其他'
+];
+
+var statByArea = function(records){
+
+};
+
+var statByLocation = function(records){
+
+};
+
+var statByTimes = function(records){
+
+};
+
+var statBySum = function(records){
+
+};
+
 /* GET users listing. */
 router.get('/', function(req, res) {
   var db = new sqlite3.Database(file);
@@ -23,7 +62,7 @@ router.get('/', function(req, res) {
       });
     }, function() {
         // All done fetching records, render response
-        res.render("Companies", {title: 'companies', companies: companies});
+        res.render("Index", {title: 'companies', companies: companies});
     });
   });
 });
